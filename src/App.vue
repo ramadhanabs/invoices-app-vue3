3414cca970ef2860baaf4e9bf32a8aee5a171712
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-06-03 13:56:56 +0700

    [update] main and app

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,81 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Navbar />
+  <div id="root">
+    <router-view />
+  </div>
+  <Transition name="slide-fade">
+    <SideModal v-if="store.isOpen" />
+  </Transition>
 </template>
+<script>
+import Navbar from '@/components/Navbar/index.vue'
+import SideModal from '@/components/Modal/index.vue'
+import { defineComponent, ref } from 'vue'
+import { useModalStore } from '@/stores/modal.js'
+export default defineComponent({
+  components: { Navbar, SideModal },
+  setup() {
+    const store = useModalStore()
 
+    return { store }
+  }
+})
+</script>
 <style>
-@import './assets/base.css';
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #141625 !important;
+  font-family: 'Poppins', sans-serif !important;
+}
+
+p {
+  color: white;
+  margin-bottom: 0px !important;
+}
+
+#root {
+  display: flex;
+  justify-content: center;
+  padding: 48px;
+  height: 100%;
+}
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+  height: 100%;
 }
 
-header {
-  line-height: 1.5;
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.slide-fade-leave-active {
+  transition: all 0.3s ease-out;
 }
 
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  opacity: 0;
 }
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+.ant-progress-inner {
+  background-color: #3c3054 !important;
 }
 
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
+.ant-progress-bg {
+  background-color: #8066b5 !important;
+}
 
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
+input[type='number']::-webkit-outer-spin-button,
+input[type='number']::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
 
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+input[type='number'] {
+  -moz-appearance: textfield;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import router from './router'
+
+const app = createApp(App)
+app.use(router).use(Antd).use(createPinia()).mount('#app')
