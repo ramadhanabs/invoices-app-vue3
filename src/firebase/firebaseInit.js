f77e2445cfc7470251650e35168a24077c72f61a
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-06-04 16:02:49 +0700

    [config] firebase

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAcX4eMPK5OQIFcYAk8Jicjd8e8TrG85bE',
+  authDomain: 'invoice-app-vue-a22b9.firebaseapp.com',
+  projectId: 'invoice-app-vue-a22b9',
+  storageBucket: 'invoice-app-vue-a22b9.appspot.com',
+  messagingSenderId: '1061091480303',
+  appId: '1:1061091480303:web:a50757b9bfbb0d65dc411a'
+}
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+console.log(firebaseApp)
+
+export default firebaseApp.firestore()
