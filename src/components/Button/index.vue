01db3f209203355902585e9b1e572ff8164a9432
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-06-05 15:08:20 +0700

    [update] button

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -1,17 +1,46 @@
 <template>
-  <a-button v-bind="$attrs"><slot></slot></a-button>
+  <a-button :class="type && `${type}-button`" v-bind="$attrs"
+    ><slot></slot
+  ></a-button>
 </template>
+<script>
+export default {
+  props: {
+    type: String
+  }
+}
+</script>
 <style scoped>
 .ant-btn {
-  border-color: #8066b5 !important;
-  background-color: #3c3054 !important;
+  border-color: #8066b5;
+  background-color: #3c3054;
   color: white;
-  padding: 16px 24px !important;
-  line-height: 1 !important;
+  padding: 16px 24px;
+  line-height: 1;
   border-radius: 8px;
   height: auto;
 }
 .ant-btn:hover {
-  background-color: #2a213c !important;
+  background-color: #2a213c;
+}
+
+.success-button {
+  background-color: rgba(172, 255, 47, 0.259) !important;
+  border-color: greenyellow !important;
+  color: greenyellow !important;
+}
+
+.success-button:hover {
+  background-color: rgb(173, 255, 47, 0.1) !important;
+}
+
+.error-button {
+  background-color: rgb(128, 0, 0, 0.5) !important;
+  border-color: red !important;
+  color: red !important;
+}
+
+.error-button:hover {
+  background-color: rgba(128, 0, 0, 0.2) !important;
 }
 </style>
