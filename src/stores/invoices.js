e9616115f6242be1dc7765ce22bc45ed19462801
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-06-26 18:05:41 +0700

    [update] get data by user id

--- src/stores/invoices.js
+++ src/stores/invoices.js
@@ -9,24 +9,32 @@ export const useInvoiceStore = defineStore({
     isLoading: false
   }),
   actions: {
-    async fetchData() {
+    /* 
+      Improve this methods using firebase query. 
+
+      CURRENT => Still fetching all data then .filter() it based on userId
+      EXPECTED IMPROVEMENT => Using firebase query to get data based on userId
+    */
+    async fetchData(user_id) {
       this.isLoading = true
       const getData = db.collection('invoices')
       const snapshot = await getData.get()
-      this.data = snapshot.docs.map((doc) => {
-        const tempData = doc.data()
-        const { id, dueDate, billTo, status, totalAmount } = tempData
-        return {
-          ...tempData,
-          preview: {
-            id,
-            dueDate: dayjs(dueDate).format('MMMM D, YYYY'),
-            status,
-            amount: totalAmount,
-            biller: billTo.name
+      this.data = snapshot.docs
+        .filter((o) => o.data().userId === user_id)
+        .map((doc) => {
+          const tempData = doc.data()
+          const { id, dueDate, billTo, status, totalAmount } = tempData
+          return {
+            ...tempData,
+            preview: {
+              id,
+              dueDate: dayjs(dueDate).format('MMMM D, YYYY'),
+              status,
+              amount: totalAmount,
+              biller: billTo.name
+            }
           }
-        }
-      })
+        })
       this.isLoading = false
     }
   }
